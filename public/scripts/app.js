49e55835ea978b6e81acfe1ac72f13a36e24fd48
RobertoC27 <chi13027@uvg.edu.gt>
2016-07-28 21:41:43 -0600

    ya funciona el reloj digital
    se actualiza el reloj digital y hace todas sus funciones correctamente

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,55 +7,109 @@ state = 2: green
 */
 
 // Modelo / estado
+
 var state = 0;
 
-var viewport = document.getElementById("viewport");
-var changeTrigger = document.getElementById("change");
+var hour1=2;
+var hour2=2;
+var minute1=5;
+var minute2=9;
+var seconds=58;
 
-changeTrigger.addEventListener("click", function(){
-  if(state === 0){
-    state = 2;
-  }
-  else if(state === 1){
-    state = 0;
-  }
-  else{
-    state = 1;
-  }
 
-  viewport.innerHTML = render(state);
+var canvas=document.getElementById("test");
+
+var clockController= document.getElementById("selector");
+
+clockController.addEventListener("change", function() {
+    if (clockController.value=== "digital") {
+      state=0;
+    }else if(clockController.value === "analogo"){
+      state=1;
+    }
+    canvas.innerHTML = render2(state);
 });
 
+var changeTrigger = document.getElementById("change");
 
-function render(state){
+function render2(state){
   var html = "";
+  if (state==0) {
+    html += '<div class="display">';
+    html += '<div class="number hour1">'+ hour1 +'</div>';
+    html += '<div class="number hour2">'+ hour2 +'</div>';
+    html += '<div class="separation">'+ seconds +'</div>';
+    html += '<div class="number minute1">'+ minute1 +'</div>';
+    html += '<div class="number minute2">'+ minute2 +'</div>';
+    html += '<div style="clear:both;"></div>';
+    html += '</div>';
 
-  html += '<div class="traffic-light">';
+    seconds+=1;
+    if(seconds===60){
+      seconds=0;
+      minute2+=1;
+    }
+    if(minute2>9){
+      minute1+=1;
+      minute2=0;
+    }
+    if (minute1>5) {
+      hour2+=1;
+      minute1=0;
+      minute2=0;
+    }
+    if (hour1===2) {
+      if (hour2>3) {
+        hour2=0;
+        hour1=0;
+        minute2=0;
+        minute1=0;
+      }
+    }
 
-  if(state === 0){
-    html += '<div class="red light"></div>';
   }
-  else{
-    html += '<div class="red light off"></div>';
-  }
+  else if (state===1) {
 
-  if(state === 1){
-    html += '<div class="yellow light"></div>';
-  }
-  else{
-    html += '<div class="yellow light off"></div>';
-  }
+  	html += '<div id="analog-clock">';
 
-  if(state === 2){
-    html += '<div class="green light"></div>';
-  }
-  else{
-    html += '<div class="green light off"></div>';
-  }
+    html += '<div class="digit 1">1</div>';
+    html += '<div class="digit 2">2</div>';
+    html += '<div class="digit 3">3</div>';
+    html += '<div class="digit 4">4</div>';
+    html += '<div class="digit 5">5</div>';
+    html += '<div class="digit 6">6</div>';
+    html += '<div class="digit 7">7</div>';
+    html += '<div class="digit 8">8</div>';
+    html += '<div class="digit 9">9</div>';
+    html += '<div class="digit 10">10</div>';
+    html += '<div class="digit 11">11</div>';
+    html += '<div class="digit 12">12</div>';
 
-  html += '</div>';
+    html += '<div class="hourteller">';
+    html += '<div class="show hour"></div>';
+    html += '<div class="no-show hour"></div>';
+    html += '</div>';
 
-  return html;
+
+    html += '<div class="minuteteller">';
+    html += '<div class="show minute"></div>';
+    html += '<div class="no-show minute"></div>';
+    html += '</div>';
+
+
+    html += '<div class="secondteller">';
+    html += '<div class="show second"></div>';
+    html += '<div class="no-show second"></div>';
+    html += '</div>';
+
+    html += '</div>';
+
+  }
+  return html; 
 }
 
-viewport.innerHTML = render(state);
+function r(){
+  canvas.innerHTML=render2(state);
+}
+r();
+setInterval(r,1000);
